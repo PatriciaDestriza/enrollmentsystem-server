2fe43d9bbf215d88a8ec32bf119f090815ea1570
James Jilhaney <notail2111@gmail.com>
2022-04-27 23:10:25 +0800

    Added routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,19 @@
 <?php
 
+use App\Http\Controllers\AcademicTermController;
+use App\Http\Controllers\AcademicYearController;
+use App\Http\Controllers\BlockController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EnrolledStudentController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\YearLevel;
+use App\Http\Controllers\YearLevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +34,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/signup', [UserController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
+
+Route::resource('/academic-years', AcademicYearController::class);
+Route::resource('/academic-terms', AcademicTermController::class);
+Route::resource('/departments', DepartmentController::class);
+Route::resource('/year-levels', YearLevelController::class);
+Route::resource('/schedules', ScheduleController::class);
+Route::resource('/teachers', TeacherController::class);
+Route::resource('/programs', ProgramController::class);
+Route::resource('/rooms', RoomController::class);
+Route::resource('/courses', CourseController::class);
+Route::resource('/student', StudentController::class);
+Route::resource('/enrolled-students', EnrolledStudentController::class);
+Route::resource('/blocks', BlockController::class);
